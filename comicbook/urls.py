d3b25f0a684c663340c20531f58b641cfcecb14d
localduck <duck.green.green@gmail.com>
2020-09-22 12:49:03 +0700

    correct setup custom error page 404

--- comicbook/urls.py
+++ comicbook/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
+from mainapp import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
